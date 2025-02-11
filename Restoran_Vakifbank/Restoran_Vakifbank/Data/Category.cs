110209828c552b272d05aad07a78dbd3b653401c
Mustafa Akdaş <makdas163@gmail.com>
2025-02-11 17:50:53 +0300

    Restoran Projesine dönüldü

---
+++ Restoran_Vakifbank/Restoran_Vakifbank/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restoran_Vakifbank.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Plase enter name..")]
+        public string Name { get; set; } = "";
+    }
+}
---
+++ Restoran_Vakifbank/Restoran_Vakifbank/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Restoran_Vakifbank.Data;
+
+namespace Restoran_Vakifbank.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Category Create(Category obj);
+        public Category Update(Category obj);
+        public bool Delete(int obj);
+        public Category Get(int id);
+        public IEnumerable<Category> GetAll();
+    }
+}
